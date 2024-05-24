aa88419d324ae0086079a1051bc533ca23df2a9a
Kamila Sproska <kamila.sproska@gmail.com>
2024-05-24 13:22:09 +0200

    displaying percentage of methods for MI threshold value part 2

--- display_percentage_for_thresholds.py
+++ display_percentage_for_thresholds.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -11,7 +12,7 @@ data['Microsoft Maintainability Index (MI)'] = pd.to_numeric(
 total_records = len(data)
 mi_percentages = {}
 
-for mi_value in range(10, 41):
+for mi_value in range(10, 51):
     filtered_data = data[data['Microsoft Maintainability Index (MI)'] <= mi_value]
     mi_percentages[mi_value] = (len(filtered_data) / total_records) * 100
 
@@ -19,10 +20,30 @@ mi_values = list(mi_percentages.keys())
 percentages = list(mi_percentages.values())
 
 plt.figure(figsize=(10, 6))
-plt.plot(mi_values, percentages, color='blue', marker='.')
+plt.plot(mi_values, percentages, color='blue', marker='.', linestyle='--', linewidth=0.6)
+
+for mi, percentage in zip(mi_values, percentages):
+    if mi % 5 == 0:
+        color = 'darkblue'
+        if mi == 10:
+            color = 'red'
+        if mi == 20:
+            color = 'orange'
+        plt.plot(mi, percentage, color=color, marker='o', linestyle='')
+        plt.annotate(f'{percentage:.3f}%',
+                     (mi, percentage),
+                     textcoords="offset points",
+                     xytext=(0, 10),
+                     ha='center')
+
 plt.xlabel('Microsoft Maintainability Index (MI)')
-plt.ylabel('Percentage of Records (%)')
-plt.title('Percentage of Records by MI Value')
-plt.xticks(range(10, 41, 5))
+plt.ylabel('Percentage of Records below threshold MI value (%)')
+plt.title('Cumulative Percentage of Records by MI Value')
+plt.xticks(range(10, 51, 5))
 plt.grid(True, which='both', linestyle='--', linewidth=0.3)
-plt.show()
+
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+filename = f'./plots/plot_cumulative_percentage_by_mi_{timestamp}.png'
+
+plt.savefig(filename)
+print(f'Plot saved as {filename}')
