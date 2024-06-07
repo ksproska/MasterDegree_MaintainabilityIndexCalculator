40ca29091f8b834353aa559489f6daf927afab1f
Kamila Sproska <kamila.sproska@gmail.com>
2024-06-07 22:53:41 +0200

    final display of ROC curve diagrams

--- roc_curve/plot_roc_curve.py
+++ roc_curve/plot_roc_curve.py
@@ -13,16 +13,15 @@ metrics = [
 ]
 
 translation_map = {
-    'Halstead Volume': 'Objętość Halsteada',
-    'Cyclomatic Complexity (CC)': 'Złożoność Cyklomatyczna (CC)',
-    'Lines Of Code (LOC)': 'Liczba Linii Kodu (LOC)',
-    'Microsoft Maintainability Index (MI)': 'Indeks Utrzymywalności (MI)',
-    'Maintainability Index (MI) - calculated': 'Wyliczony Indeks Utrzymywalności (MI)'
+    'Halstead Volume': 'Objętości Halsteada',
+    'Cyclomatic Complexity (CC)': 'Złożoności Cyklomatycznej (CC)',
+    'Lines Of Code (LOC)': 'Liczby Linii Kodu (LOC)',
+    'Microsoft Maintainability Index (MI)': 'Indeksu Utrzymywalności (MI) (odwrócona)'
 }
 
 
 def calculate_maintainability_index(halstead_volume, cc, loc):
-    return int(max(0, (171.0 - 5.2 * math.log(halstead_volume) - 0.23 * cc - 16.2 * math.log(loc)) * (100.0 / 171.0)))
+    return int(max(0, (171 - 5.2 * math.log(halstead_volume) - 0.23 * cc - 16.2 * math.log(loc)) * (100 / 171)))
 
 
 def main():
@@ -39,22 +38,25 @@ def main():
     for i, metric in enumerate(metrics, 1):
         X = data[metric].values.reshape(-1, 1)
 
+        if metric == 'Microsoft Maintainability Index (MI)':
+            X = -X  # Reverse the scoring for MI, as higher scores are better
+
         fpr, tpr, thresholds = roc_curve(y, X)
         roc_auc = auc(fpr, tpr)
 
         optimal_idx = np.argmax(tpr - fpr)
-        optimal_threshold = thresholds[optimal_idx]
+        optimal_threshold = abs(thresholds[optimal_idx])
         calculated_thresholds[metric] = optimal_threshold
 
         plt.subplot(2, 2, i)
-        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
-        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'AUC = {roc_auc:.2f}')
+        plt.plot([0, 1], [0, 1], color='black', lw=1, linestyle='--')
         plt.scatter(fpr[optimal_idx], tpr[optimal_idx], marker='o', color='black',
-                    label=f'Optimal Threshold = {optimal_threshold:.2f}')
+                    label=f'Wartość brzegowa = {optimal_threshold:.1f}')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
-        plt.xlabel('Współczynnik False Positive')
-        plt.ylabel('Współczynnik True Positive')
+        plt.xlabel('FPR')
+        plt.ylabel('TPR')
         plt.title(f'Krzywa ROC dla {translation_map[metric]}')
         plt.legend(loc="lower right")
 
